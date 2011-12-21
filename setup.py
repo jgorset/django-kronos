5e0c98c20ec313a16432964ccd9514a150e15739
Johannes Gorset <jgorset@gmail.com>
2011-12-21 14:57:09 +0100

    Bump version to 0.2.2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name = 'kronos',
-    version = '0.2.1',
+    version = '0.2.2',
     description = 'Kronos is a Django application that makes it easy to define and schedule tasks with cron.',
     author = 'Johannes Gorset',
     author_email = 'jgorset@gmail.com',

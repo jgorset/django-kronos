668f979046cd80c7e8e1e9781238c4f67da1e9f5
Johannes Gorset <jgorset@gmail.com>
2011-12-26 11:18:59 +0100

    Change package name

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(
-    name = 'kronos',
+    name = 'django-kronos',
     version = '0.2.2',
     description = 'Kronos is a Django application that makes it easy to define and schedule tasks with cron.',
     author = 'Johannes Gorset',

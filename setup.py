c0d2849fc012afd7333d496e7df1f0e5fbb04bb9
Johannes Gorset <jgorset@gmail.com>
2011-12-28 13:47:35 +0100

    Add long description

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setup(
     name = 'django-kronos',
     version = '0.2.2',
     description = 'Kronos is a Django application that makes it easy to define and schedule tasks with cron.',
+    long_description = open('README.md').read() + '\n\n' + open('CHANGELOG').read(),
     author = 'Johannes Gorset',
     author_email = 'jgorset@gmail.com',
     url = 'http://github.com/jgorset/kronos',

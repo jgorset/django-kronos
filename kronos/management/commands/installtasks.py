82f656bc68ac653037d2ab9fd18d0752fa704964
Johannes Gorset <jgorset@gmail.com>
2012-02-24 20:49:52 +0100

    Remove --fake option from installtasks

--- kronos/management/commands/installtasks.py
+++ kronos/management/commands/installtasks.py
@@ -8,20 +8,6 @@ from kronos import tasks, reinstall, printtasks
 class Command(NoArgsCommand):
     help = 'Register tasks with cron'
 
-    option_list = NoArgsCommand.option_list + (
-        make_option(
-            '--fake',
-            action='store_true',
-            dest='fake',
-            default=False,
-            help='Do not write the crontab, instead print the lines that \
-                  would be added for debugging'
-        ),
-    )
-
     def handle_noargs(self, **options):
-        if options.get('fake'):
-            printtasks()
-        else:
-            reinstall()
-            print "Installed %s tasks." % len(tasks)
+        reinstall()
+        print "Installed %s tasks." % len(tasks)

74fe7ff33b334c95302e5c410975340978791315
Rodrigo Nicolas Carreras <carrerasrodrigo@gmail.com>
2014-10-15 19:12:45 -0300

    Change the testing system Added the option of listing all the registered tasks Updated the setup

--- tests/project/app/views.py
+++
@@ -1,7 +0,0 @@
-from django.http import HttpResponse
-
-from fandjango.decorators import facebook_authorization_required
-
-@facebook_authorization_required()
-def home(request):
-    return HttpResponse()

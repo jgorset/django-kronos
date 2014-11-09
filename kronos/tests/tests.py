940615f9cd362cb1366b84012da44c333b9c031f
Rodrigo Nicolas Carreras <carrerasrodrigo@gmail.com>
2014-11-09 12:01:29 -0300

    Added support for registering django commands
    - Support for django commands
    - Code refactoring
    - Removed unused imports
    - Docs updated
    - Added new tests cases
    - New version
    - Added contributors file
    - Added support for python 3.4

--- kronos/tests/tests.py
+++ kronos/tests/tests.py
@@ -2,11 +2,15 @@ import sys
 import kronos.tests.project.app
 import kronos.tests.project.cron
 
-from StringIO import StringIO
+try:
+    from StringIO import StringIO
+except ImportError:
+    from io import StringIO
 from subprocess import PIPE
 
 from django.core.management import call_command
 from django.test import TestCase
+from django.core.management.base import CommandError
 from kronos import tasks, load
 from kronos.utils import read_crontab, write_crontab
 from mock import Mock, patch
@@ -26,7 +30,6 @@ class TestCase(TestCase):
         )
 
         call_command('uninstalltasks')
-
         self.assertTrue(mock.called)
 
     @patch('subprocess.Popen')
@@ -86,14 +89,21 @@ class TestCase(TestCase):
     def test_task_collection(self):
         """Test task collection."""
         self.assertIn(kronos.tests.project.app.cron.complain.__name__,
-            [task.__name__ for task in tasks])
+            [task['name'] for task in tasks])
         self.assertIn(kronos.tests.project.cron.praise.__name__,
-            [task.__name__ for task in tasks])
+            [task['name'] for task in tasks])
+        self.assertIn('task',
+            [task['name'] for task in tasks])
 
     def test_runtask(self):
         """Test running tasks via the ``runtask`` command."""
         call_command('runtask', 'complain')
         call_command('runtask', 'praise')
+
+    def test_runtask_django(self):
+        """Test running tasks via the ``runtask`` command."""
+        self.assertRaises(CommandError,
+            lambda: call_command('runtask', 'task'))
 
     @patch('subprocess.Popen')
     def test_installtasks(self, mock):
@@ -107,6 +117,20 @@ class TestCase(TestCase):
 
         self.assertTrue(mock.called)
 
+    @patch('subprocess.Popen')
+    def test_installed_tasks(self, mock):
+        """Test installing tasks with the ``installtasks`` command."""
+        mock.return_value = Mock(
+            stdout=StringIO('crontab: installing new crontab'),
+            stderr=StringIO('')
+        )
+
+        call_command('installtasks')
+        calls = str(mock.mock_calls[-1])
+        self.assertIn('runtask praise', calls)
+        self.assertIn('runtask complain', calls)
+        self.assertIn('manage.py task', calls)
+
     def test_list_tasks(self):
         sys.stdout = StringIO()
         call_command('showtasks')
@@ -114,3 +138,4 @@ class TestCase(TestCase):
         val = sys.stdout.read()
         self.assertIn('complain', val)
         self.assertIn('praise', val)
+        self.assertIn('task', val)

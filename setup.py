b56bf2c93bf82831a8ac5ffa9247da93e232cf6c
Stefan Foulis <stefan@foulis.ch>
2016-01-20 00:25:00 +0100

    fix github link

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     long_description=readme + '\n\n' + history,
     author='Johannes Gorset',
     author_email='jgorset@gmail.com ',
-    url='http://github.com/jgorset/kronos',
+    url='http://github.com/jgorset/django-kronos',
     packages=['kronos'],
     include_package_data=True,
     zip_safe=False,
